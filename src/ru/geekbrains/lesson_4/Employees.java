97f58c839b49ddda257c21c93b822f7e5456114a
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-08-23 20:08:11 +0500

    finish 1, 2, 3 task

---
+++ src/ru/geekbrains/lesson_4/Employees.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.lesson_4;
+
+public class Employees {
+    String fullName;
+    String position;
+    String email;
+    int telephone;
+    double salary;
+    int age;
+
+    public Employees(String _fullName, String _position, String _email, int _telephone, double _salary, int _age){
+
+        fullName = _fullName;
+        position = _position;
+        email = _email;
+        telephone = _telephone;
+        salary = _salary;
+        age = _age;
+
+    }
+
+    public void getEmployees(){
+        System.out.println(this.fullName + ", " + this.position + ", " + this.email + ", " + this.telephone + ", " + this.salary + ", " + this.age);
+    }
+}
---
+++ src/ru/geekbrains/lesson_4/Main.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson_4;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Employees employees1 = new Employees("Kolesnikov Yu. S.", "chief specialist", "qwe@qwe.ru", 1234567890, 123456, 28);
+        employees1.getEmployees();
+    }
+}
